2934d14cbcec33d32a3896fa2889d8d5e538028e
Jake Prins <jakeprins@outlook.com>
2020-06-12 14:38:07 +0200

    Setup layout of header for landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,9 @@
-export default function Home() {
-  return (
-    <div className='md:flex bg-white rounded-lg p-6'>
-      <img
-        className='h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6'
-        src='avatar.jpg'
-      />
-      <div className='text-center md:text-left'>
-        <h2 className='text-lg'>Erin Lindford</h2>
-        <div className='text-purple-500'>Customer Support</div>
-        <div className='text-gray-600'>erinlindford@example.com</div>
-        <div className='text-gray-600'>(555) 765-4321</div>
-      </div>
-    </div>
-  );
+import Header from 'components/layout/Header';
+
+export default function Home(): JSX.Element {
+	return (
+		<>
+			<Header />
+		</>
+	);
 }
