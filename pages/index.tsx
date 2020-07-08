1e8040570c1bb191c03e500917cfda76ff27b255
Jake Prins <jakeprins@outlook.com>
2020-07-08 20:35:47 +0200

    Test initial setup NetlifyCMS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage, GetStaticProps } from 'next';
+import Head from 'next/head';
 
 import { Client, Prismic } from 'config/prismic';
 import { Post } from 'interfaces/post';
@@ -10,20 +11,40 @@ import StepsSection from 'components/home/StepsSection';
 import TeamSection from 'components/home/TeamSection';
 import PricingSection from 'components/home/PricingSection';
 
+import { attributes, react as HomeContent } from '../content/home.md';
+
 interface Props {
   posts: Post[];
 }
 
 const HomePage: NextPage<Props> = ({ posts }) => {
+  const { title, cats } = attributes;
   return (
-    <Layout>
-      <HeroSection />
-      <FeatureSection />
-      <StepsSection />
-      <PricingSection />
-      <TeamSection />
-      <BlogSection posts={posts} />
-    </Layout>
+    <>
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <article>
+        <h1>{title}</h1>
+        <HomeContent />
+        <ul>
+          {cats.map((cat, k) => (
+            <li key={k}>
+              <h2>{cat.name}</h2>
+              <p>{cat.description}</p>
+            </li>
+          ))}
+        </ul>
+      </article>
+      <Layout>
+        <HeroSection />
+        <FeatureSection />
+        <StepsSection />
+        <PricingSection />
+        <TeamSection />
+        <BlogSection posts={posts} />
+      </Layout>
+    </>
   );
 };
 
