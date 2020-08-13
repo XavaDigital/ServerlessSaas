43696793e7cf1fafc9d9f1354ce4307b1e5f2dc1
Jake Prins <jakeprins@outlook.com>
2020-08-13 11:53:22 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,11 @@ import { AppProps } from 'next/app';
 
 import '../css/index.css';
 import { AuthProvider } from '../hooks/useAuth';
-import { TeamProvider } from 'context/team-context';
 
 export default function App({ Component, pageProps }: AppProps): any {
   return (
     <AuthProvider>
-      <TeamProvider>
-        <Component {...pageProps} />
-      </TeamProvider>
+      <Component {...pageProps} />
     </AuthProvider>
   );
 }
