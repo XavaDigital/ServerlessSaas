556d65f98b5daf4c49e462c0c847723f2fc9f358
Jake Prins <jakeprins@outlook.com>
2020-09-08 16:55:45 +0200

    Fix logo header link

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -6,15 +6,11 @@ const Header = (): JSX.Element => {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <Link href="/">
           <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <Link href="/dashboard">
-              <a href="" className="flex">
-                <img
-                  className="h-8 w-auto sm:h-10"
-                  src="/img/logo.png"
-                  alt="Serverless SaaS Boilerplate"
-                />
-              </a>
-            </Link>
+            <img
+              className="h-8 w-auto sm:h-10"
+              src="/img/logo.png"
+              alt="Serverless SaaS Boilerplate"
+            />
           </a>
         </Link>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
