da8239d127bba0e6c50ad04b880f84912d38401a
Jake Prins <jakeprins@outlook.com>
2020-09-18 10:46:43 +0200

    Fix purged css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   purge: {
     mode: 'all',
     content: [
-      './pages/**/*.{js,jsx,ts,tsx}',
-      './components/**/*.{js,jsx,ts,tsx}',
+      './src/pages/**/*.{js,jsx,ts,tsx}',
+      './src/components/**/*.{js,jsx,ts,tsx}',
       './next.config.js',
     ],
     options: {
