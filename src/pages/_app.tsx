ef5d3ed98069cb7eff515f2ecfe05e844a6b871b
Jake Prins <jakeprins@outlook.com>
2020-12-07 20:33:49 +0100

    Implement useToast hook and Toast component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import { AppProps } from 'next/app';
 
 import { AuthProvider } from 'hooks/useAuth';
 import { TeamProvider } from 'hooks/useTeam';
+import { ToastProvider } from 'hooks/useToast';
 import 'css/tailwind.css';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <AuthProvider>
       <TeamProvider>
-        <Component {...pageProps} />
+        <ToastProvider>
+          <Component {...pageProps} />
+        </ToastProvider>
       </TeamProvider>
     </AuthProvider>
   );
